4d82bc54d31ed1e234a84b2a78df7a7453d3f621
Himanshu Singh <138711578+himansh025@users.noreply.github.com>
2025-07-31 21:52:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,13 @@ const connectDB = async () => {
       process.exit(1); // Exit with failure
   }
 };
+
 connectDB()
 
+// 👇 Root route
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
