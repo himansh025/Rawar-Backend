fde10aaedeb7a44affe3e0d553c7b0590ae100bf
Himanshu Singh <138711578+himansh025@users.noreply.github.com>
2025-12-28 14:46:41 +0530

    Implement health check endpoint
    Add health check endpoint to the API.

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ import express from 'express';
 import cookieParser from "cookie-parser";
 import cors from 'cors';
 const app = express();
-
+app.use("/api/health",async(req,res)=>{
+  res.send("working fine"));
 const allowedOrigins =['https://rawar-frontend.vercel.app'];
 app.use(cors({
   origin: (origin, callback) => {
